9ebac93b9dc70e619219990f855aecc32318cb01
Boien <59290906+jonagull@users.noreply.github.com>
2025-08-22 19:13:06 +0200

    made attacks semi work, more level stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
 @onready var anim_sprite = $AnimatedSprite2D
-@onready var health_component = $HealthComponent
-@onready var tree_detection = $TreeDetectionArea
-@onready var stone_detection = $StoneDetectionArea
-@onready var hud = get_node("/root/Main/HUD")
 @export var speed := 100
 
 var last_direction := "down"
+var is_attacking := false
 
 func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS
-
+	# Connect animation finished signal
+	print("ready player one")
+	anim_sprite.connect("animation_finished", Callable(self, "_on_attack_finished"))
 
 func _physics_process(_delta):
+	#print(is_attacking)
 	var input_vector = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
@@ -25,27 +24,38 @@ func _physics_process(_delta):
 
 		# Determine direction
 		if abs(input_vector.x) > abs(input_vector.y):
-			if input_vector.x > 0:
-				last_direction = "right"
-			else:
-				last_direction = "left"
+			last_direction = "right" if input_vector.x > 0 else "left"
 		else:
-			if input_vector.y > 0:
-				last_direction = "down"
-			else:
-				last_direction = "up"
+			last_direction = "down" if input_vector.y > 0 else "up"
 
-		anim_sprite.animation = "walk_" + last_direction
-		anim_sprite.play()
+		# Only change animation if not attacking
+		if not is_attacking:
+			anim_sprite.animation = "walk_" + last_direction
+			anim_sprite.play()
 	else:
 		velocity = Vector2.ZERO
-		anim_sprite.animation = "idle_" + last_direction
-		anim_sprite.play()
-	
+		if not is_attacking:
+			anim_sprite.animation = "idle_" + last_direction
+			anim_sprite.play()
+
 func _unhandled_input(event):
-	print(event)
-	pass
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		attack()
 
+func attack():
+	if is_attacking:
+		return
+	is_attacking = true
+	var attack_anim = "chop_" + last_direction
+	
+	if anim_sprite.sprite_frames.has_animation(attack_anim):
+		anim_sprite.animation = attack_anim
+		anim_sprite.play()
+	else:
+		print("Missing attack animation:", attack_anim)
+		is_attacking = false
 
-func _process(delta):
-	pass
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print("finished")
+	is_attacking = false
+	#pass # Replace with function body.
